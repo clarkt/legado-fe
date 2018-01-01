7c13fb73df3edd7f39cb82eb7cf09efb0d551338
Truls Clark <truls.clark@gmail.com>
2018-01-01 15:37:36 +0100

    added bulma

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
+import Frontpage from '@/components/Frontpage';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: 'helloworld',
       name: 'HelloWorld',
       component: HelloWorld,
+    },
+    {
+      path: '/',
+      name: 'Frontpage',
+      component: Frontpage,
     },
   ],
 });
