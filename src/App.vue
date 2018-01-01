aeae5a2652e0495fad48432d2974033a4a17cab9
Truls Clark <truls.clark@gmail.com>
2018-01-01 16:13:46 +0100

    changed template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+<section class="section">
+    <div class="container">
+      <h1 class="title">
+        Hello World
+      </h1>
+      <p class="subtitle">
+        My first website with <strong>Bulma</strong>!
+      </p>
+    </div>
+  </section>
     <router-view/>
   </div>
 </template>
